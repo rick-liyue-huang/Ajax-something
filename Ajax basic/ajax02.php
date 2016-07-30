257d37a35d9d49cffdb74ea776b78d8230554005
rickliyuehuang <rick.liyue.huang@gmail.com>
2016-07-31 02:11:34 +1000

    delete useless folders

--- Ajax basic/ajax02.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	echo $_POST['name'];
-	// echo "<br />";
-	echo $_POST['age'];
-
-	// echo $_GET['name'];
-	// echo $_GET['age'];
-?>
-
--- Ajax basic/ajax03.php
+++
@@ -1,3 +0,0 @@
-<?php 
-	echo 'hello: ' . $_GET['content'];
-?>
